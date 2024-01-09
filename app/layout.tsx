964246369306a2b33bcccb08ccd8ce29f6c2db6d
Sjors van Holst <sjorsvanholst@gmail.com>
2024-01-09 17:03:04 +0100

    🍱 Added Google Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,6 +53,11 @@ export default async function RootLayout({
           <meta name="msapplication-config" content="/browserconfig.xml" />
           <meta name="theme-color" content="#ffffff" /> 
           */}
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Open+Sans:wght@400;600&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet"
+          />
         </head>
         <body>
           {children}
