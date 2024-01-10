327db2314d47dbb3734390d4065480c071919eb2
Sjors van Holst <sjorsvanholst@gmail.com>
2024-01-10 22:24:27 +0100

    ✨ Added restoration feature

--- components/Intro/index.tsx
+++ components/Intro/index.tsx
@@ -11,24 +11,36 @@ import Results from "@/components/Results";
 import Background from "@/components/Background";
 
 import { Answer, Company, Question } from "@/types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { generateQuestion } from "../../api/open-ai";
+
+const QUESTION_LIMIT = 6;
 
 type IntroProps = {
   initialAnswers: Answer[];
   initialQuestions: Question[];
 };
 
-export default function Intro({
-  initialAnswers,
-  initialQuestions,
-}: IntroProps) {
+export default function Intro({ initialAnswers, initialQuestions }: IntroProps) {
   const [index, setIndex] = useState(initialAnswers.length);
   const [answers, setAnswers] = useState(initialAnswers);
   const [questions, setQuestions] = useState(initialQuestions);
 
   const questionsCombined = [...defaultQuestions, ...questions] as Question[];
   const questionCurrent = questionsCombined[index];
+
+  const getCompany = (answers: Answer[]) => {
+    if (answers.length < 3) {
+      throw new Error("Not enough answers to generate company");
+    }
+
+    // I'm assuming these answers are present since these are the first three questions
+    const name = answers.find((answer) => answer.uuid === "4dfb8b90-a70e-47cc-a9f2-51608f86d04c")!.value;
+    const activity = answers.find((answer) => answer.uuid === "0db64dd3-5440-49a4-9252-c0d8ba49fa62")!.value;
+    const mission = answers.find((answer) => answer.uuid === "76253f31-0daf-4fa5-908e-6538f7da5c16")!.value;
+
+    return { name, activity, mission };
+  };
 
   const handleAnswer = async (answer: string, question: Question) => {
     const { uuid } = question;
@@ -41,33 +53,22 @@ export default function Intro({
 
     Cookies.set("answers", answersParsed);
 
+    // If the user has answered the first three questions we can generate more questions
     if (answersUpdated.length === 3) {
-      const name = answersUpdated.find(
-        (answer) => answer.uuid === "4dfb8b90-a70e-47cc-a9f2-51608f86d04c"
-      )!.value;
+      const company = getCompany(answersUpdated);
+      const index = 0;
 
-      const activity = answersUpdated.find(
-        (answer) => answer.uuid === "0db64dd3-5440-49a4-9252-c0d8ba49fa62"
-      )!.value;
-
-      const mission = answersUpdated.find(
-        (answer) => answer.uuid === "76253f31-0daf-4fa5-908e-6538f7da5c16"
-      )!.value;
-
-      handleCompletion({ name, activity, mission }, 0);
+      handleCompletion(company, index);
     }
   };
 
-  const handleCompletion = async (
-    company: Company,
-    index: number,
-    asked: string[] = []
-  ) => {
-    if (index >= 6) {
+  // This recursive function will generate questions until the limit is reached
+  const handleCompletion = async (company: Company, index: number, asked: string[] = []) => {
+    if (index >= QUESTION_LIMIT) {
       return;
     }
 
-    const question = await generateQuestion(company, index, 6, asked);
+    const question = await generateQuestion(company, index, QUESTION_LIMIT, asked);
     const questionValue = question.question;
 
     setQuestions((questions) => {
@@ -85,16 +86,24 @@ export default function Intro({
     handleCompletion(company, index, asked);
   };
 
+  // In case the user closed the tab mid-way through the question generation process we'll resume it
+  useEffect(() => {
+    if (questions.length < QUESTION_LIMIT && answers.length >= 3) {
+      const index = questions.length;
+      const company = getCompany(answers);
+      const asked = questions.map((question) => question.question);
+
+      handleCompletion(company, index, asked);
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
       <Background color={questionCurrent?.color} />
 
       <div className={styles.main__content}>
         {questionCurrent ? (
-          <Input
-            question={questionCurrent}
-            onAnswer={(answer) => handleAnswer(answer, questionCurrent)}
-          />
+          <Input question={questionCurrent} onAnswer={(answer) => handleAnswer(answer, questionCurrent)} />
         ) : (
           <Results />
         )}
