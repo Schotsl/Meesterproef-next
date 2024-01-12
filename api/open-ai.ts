728276a56621f35eb68ae90aeb4089b7b1a8efae
Sjors van Holst <sjorsvanholst@gmail.com>
2024-01-12 13:49:07 +0100

    ♻️ Moved prompt to txt

--- api/open-ai.ts
+++ api/open-ai.ts
@@ -1,23 +1,9 @@
 "use server";
 
+import { readFileSync } from "fs";
 import { Company, Question } from "../types";
 
 import questionArray from "../examples.json";
-
-const questionObject = JSON.stringify(questionArray[0]);
-
-let startPrompt = "";
-
-// prettier-ignore
-startPrompt += "We gaan meeslepende vragen ontwerpen voor een 'kies je eigen avontuur'-spel, gefocust op het opzetten en uitbouwen van je eigen onderneming. Je ontvangt basisinformatie zoals de bedrijfsnaam, bedrijfsactiviteiten en de missie. Daarnaast moet je voor elke vraag een korte titel bedenken die de vraag samenvat.\n";
-// prettier-ignore
-startPrompt += "De vragen zullen een verhaal ontwikkelen, beginnend bij de oprichting van het bedrijf of en evolueren naar een climax, zoals een overnamebod. Het spel stimuleert spelers om na te denken over verschillende bedrijfsmodellen - van traditionele winstgerichte modellen tot maatschappelijk verantwoorde alternatieven zoals Worker Cooperatives of Steward Ownership met aandacht voor sociale impact en klimaatactie.\n";
-// prettier-ignore
-startPrompt += "Belangrijke bedrijfs momenten worden belicht, zoals bijvoorbeeld het openen van de eerste winkel, ethische besluitvorming en groeistrategieën. Het slot vraagstuk bepaalt welk bedrijfsmodel het best aansluit bij de keuzes en waarden van de speler, zoals ervaren tijdens hun reis.\n";
-// prettier-ignore
-startPrompt += "Elke vraag biedt maximaal twee keuzes, weerspiegelend diverse bedrijfsfilosofieën en -strategieën, geïnformeerd door de begin informatie over het bedrijf. Als de bedrijfsmissie nog niet bekend is hoef je hier niet naar te vragen, het systeem zelf zal deze vraag de volgende ronde stellen.\n";
-// prettier-ignore
-startPrompt += "Later worden de antwoorden op deze vragen gemeten in de metrics financiële prestatie, duurzaamheid en maatschappelijke verantwoordelijkheid en werknemerstevredenheid en -betrokkenheid. Zorg ervoor dat de impact van de verschillende vragen en keuzes duidelijk is voor de gebruiker. De vragen mogen onrealistisch of fictief zijn.\n";
 
 import OpenAI from "openai";
 
@@ -25,6 +11,9 @@ const OPENAI_KEY = process.env["OPENAI_KEY"]!;
 const OPENAI_MODEL = process.env["OPENAI_MODEL"]!;
 
 const openai = new OpenAI({ apiKey: OPENAI_KEY });
+
+const questionObject = JSON.stringify(questionArray[0]);
+const questionPrompt = readFileSync("prompt.txt", "utf8");
 
 export async function generateQuestion(
   company: Company,
@@ -38,7 +27,7 @@ export async function generateQuestion(
 
   const timeStart = performance.now();
 
-  let chatContent = startPrompt;
+  let chatContent = questionPrompt;
 
   // prettier-ignore
   chatContent += `Je hebt de taak om de ${index}e vraag in de serie te genereren. In totaal worden er ${limit} vragen gegenereerd, houd hier rekening mee ivm de opbouw van het verhaal. Een voorbeeld van het formaat en de stijl van de vragen wordt hieronder gegeven:\n`;
