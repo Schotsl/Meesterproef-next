5a815d2bf34186f3572c9fa8aa0f44c58520c179
Sjors van Holst <sjorsvanholst@gmail.com>
2024-01-18 20:51:50 +0100

    📝 Updated content

--- components/Rows/index.tsx
+++ components/Rows/index.tsx
@@ -24,8 +24,10 @@ export default function Rows() {
             <h1 className={styles.main__rows__row__content__title}>Grid & Kleur</h1>
 
             <p className={styles.main__rows__row__content__text}>
-              De meesterproef is een onderzoek naar de toekomst van de Nederlandse journalistiek. We hebben onderzocht
-              hoe de journalistiek kan veranderen om de toekomst te overleven.
+              Aan de linkerkant zie je de resultaten van het vak Grid & Kleur, waarin ik een website in Figma heb
+              ontworpen voor de verkoop van groene energie-opwekkers zoals zonnepanelen, windmolens en batterijopslag.
+              Dit project was superleuk, hoewel het wel wat kort was. Ik heb hier voor het eerst geëxperimenteerd met
+              Neo-brutalisme en ben echt trots op het eindresultaat.
             </p>
           </div>
         </section>
@@ -35,8 +37,9 @@ export default function Rows() {
             <h1 className={styles.main__rows__content__title}>Typografie</h1>
 
             <p className={styles.main__rows__content__text}>
-              De meesterproef is een onderzoek naar de toekomst van de Nederlandse journalistiek. We hebben onderzocht
-              hoe de journalistiek kan veranderen om de toekomst te overleven.
+              Voor het vak Typografie moest ik een website maken om meer te vertellen over een bepaald lettertype; ik
+              koos voor Futura. Deze website is een van mijn favorieten geworden. Ik ben vooral trots op hoe ik
+              'geëxperimenteerd' heb met het lettertype, en de website heeft een gaaf sci-fi gevoel!{" "}
             </p>
           </div>
 
@@ -58,8 +61,10 @@ export default function Rows() {
             <h1 className={styles.main__rows__content__title}>Interface & Beweging</h1>
 
             <p className={styles.main__rows__content__text}>
-              De meesterproef is een onderzoek naar de toekomst van de Nederlandse journalistiek. We hebben onderzocht
-              hoe de journalistiek kan veranderen om de toekomst te overleven.
+              In Interface & Beweging heb ik een website gemaakt waarbij je in de huid kruipt van een schurk uit
+              Scooby-Doo die de Scooby-gang bespioneert. Ik ben tevreden over hoe het is geworden, maar achteraf had ik
+              het liever in Next.js geschreven om meer nieuwe vaardigheden te leren. Toch ben ik blij met het
+              eindresultaat.
             </p>
           </div>
         </section>
@@ -69,8 +74,10 @@ export default function Rows() {
             <h1 className={styles.main__rows__content__title}>Interface & Interactie</h1>
 
             <p className={styles.main__rows__content__text}>
-              De meesterproef is een onderzoek naar de toekomst van de Nederlandse journalistiek. We hebben onderzocht
-              hoe de journalistiek kan veranderen om de toekomst te overleven.
+              Voor Interface & Interactie moesten we drie animaties maken. Ik heb mijn animaties gemaakt in Procreate
+              Dreams. Het resultaat is niet super indrukwekkend, vooral als ik bedenk hoeveel tijd ik eraan heb besteed.
+              Ik ben er niet per se 'ontevreden' mee, maar zou het niet snel aan anderen laten zien... Toch was het
+              leerzaam om meer over animatie te leren.
             </p>
           </div>
 
@@ -90,8 +97,10 @@ export default function Rows() {
             <h1 className={styles.main__rows__content__title}>Interface & Beweging</h1>
 
             <p className={styles.main__rows__content__text}>
-              De meesterproef is een onderzoek naar de toekomst van de Nederlandse journalistiek. We hebben onderzocht
-              hoe de journalistiek kan veranderen om de toekomst te overleven.
+              Tot slot heb ik voor Beeldtaal meerdere posters gemaakt. Een van de eindresultaten staat hier links,
+              waarbij ik probeer mensen te overtuigen om minder te vervuilen. Ik ben best tevreden met mijn posters,
+              maar mijn hart ligt meer bij websites en webdesign – dat zie je waarschijnlijk ook terug in het resultaat.
+              Maar het was zeker een interessant vak!"
             </p>
           </div>
         </section>
