b9c7cbb6d91ce3691ef53a73656cf2eaa2a4d891
Sjors van Holst <sjorsvanholst@gmail.com>
2024-01-18 14:16:40 +0100

    💄 Added images

---
+++ components/Results/Impact/index.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import styles from "./ResultsImpact.module.scss";
+
+import { Impact } from "../../../types";
+
+import moneyIcon from "@/public/background/profit/1.png";
+import societyIcon from "@/public/background/society/3.png";
+import workersIcon from "@/public/background/workers/2.png";
+
+type ResultsImpactProps = {
+  impact: Impact;
+};
+
+export default function ResultsImpact({ impact }: ResultsImpactProps) {
+  const societyWidth = 50 * impact.societalImpact + 50;
+  const workersWidth = 50 * impact.employeeWellbeing + 50;
+  const financialWidth = 50 * impact.financialPresentation + 50;
+
+  return (
+    <ul className={styles.impact}>
+      <li className={styles.impact__item}>
+        <Image className={styles.impact__item__icon} src={moneyIcon} alt="Money icon" />
+
+        <span className={styles.impact__item__slider} style={{ width: `${workersWidth}%` }}></span>
+      </li>
+
+      <li className={styles.impact__item}>
+        <Image className={styles.impact__item__icon} src={workersIcon} alt="Workers icon" />
+
+        <span className={styles.impact__item__slider} style={{ width: `${financialWidth}%` }}></span>
+      </li>
+
+      <li className={styles.impact__item}>
+        <Image className={styles.impact__item__icon} src={societyIcon} alt="Society icon" />
+
+        <span className={styles.impact__item__slider} style={{ width: `${societyWidth}%` }}></span>
+      </li>
+    </ul>
+  );
+}
--- components/Results/index.tsx
+++ components/Results/index.tsx
@@ -1,68 +1,94 @@
 import styles from "./Results.module.scss";
+
 import Image from "next/image";
+
+import { useQuestion } from "@/context/QuestionContext";
 import { Answer, Question } from "../../types";
+
+import ResultsImpact from "@/components/Results/Impact";
+
+import imageProfitFirst from "@/public/images/profit/1.jpg";
+import imageProfitSecond from "@/public/images/profit/2.jpg";
 
 type ResultsProps = {
   answers: Answer[];
   questions: Question[];
 };
 
-import moneyIcon from "@/public/background/profit/1.png";
-import societyIcon from "@/public/background/society/3.png";
-import workersIcon from "@/public/background/workers/2.png";
-
 export default function Results({ answers, questions }: ResultsProps) {
+  const { answersImpact } = useQuestion();
+
   return (
     <section className={styles.results}>
       <div className={styles.results__card}>
-        <h2 className={styles.results__title}>Jou uitslag!</h2>
-        <h3 className={styles.results__subtitle}>Dit is wat jij belangrijk vindt</h3>
-        <p>
-          Op basis van jouw keuzes lijkt het traditionele bedrijfsmodel het beste bij jou te passen. Wat betekent dit
-          precies?
-        </p>
+        <h2 className={styles.results__card__title}>Jou bedrijfsvorm match is een traditioneel bedrijf</h2>
 
-        <p>
-          Een traditioneel bedrijf is vaak gestructureerd rond een duidelijke hiërarchie en eigendomsverhoudingen. In
-          dit model zijn de aandeelhouders vaak de eigenaren van het bedrijf, en de besluitvorming is meestal in handen
-          van een directie of managementteam.
-        </p>
+        <ResultsImpact impact={answersImpact} />
 
-        <h3 className={styles.results__subtitle}>Kenmerken van een Traditioneel Bedrijf:</h3>
+        <section className={styles.results__card__section}>
+          <h3 className={styles.results__card__subtitle}>Dit is wat jij belangrijk vindt</h3>
+          <p>
+            Op basis van jouw keuzes lijkt het traditionele bedrijfsmodel het beste bij jou te passen. Wat betekent dit
+            precies?
+          </p>
 
-        <ul>
-          <li>
-            <b>Aandeelhouderschap</b>: De aandeelhouders bezitten het bedrijf en hun stemrecht is vaak gerelateerd aan
-            het aantal aandelen dat ze hebben.
-          </li>
-          <li>
-            <b>Winstgericht</b>: Het primaire doel is het maximaliseren van de winst voor aandeelhouders.
-          </li>
-          <li>
-            <b>Top-down Management</b>: Beslissingen worden genomen door hoger management en doorgevoerd binnen de
-            organisatie.
-          </li>
-          <li>
-            <b>Groei en Uitbreiding</b>: De focus ligt vaak op het vergroten van marktaandeel en het verhogen van de
-            omzet.
-          </li>
-          <li>
-            <b>Marktcompetitie</b>: Sterke focus op het concurreren in de markt om de beste producten of diensten te
-            leveren tegen de meest winstgevende prijzen.
-          </li>
-        </ul>
+          <p>
+            Een traditioneel bedrijf is vaak gestructureerd rond een duidelijke hiërarchie en eigendomsverhoudingen. In
+            dit model zijn de aandeelhouders vaak de eigenaren van het bedrijf, en de besluitvorming is meestal in
+            handen van een directie of managementteam.
+          </p>
+        </section>
 
-        <p>
-          Als eigenaar of manager binnen een traditioneel bedrijf heb je de kans om de strategische richting te bepalen
-          en de groei van het bedrijf te sturen. Deze bedrijfsvorm biedt structuur en duidelijke richtlijnen, wat
-          bijdraagt aan efficiëntie en effectieve besluitvorming.
-        </p>
+        <section className={styles.results__card__section}>
+          <h3>Kenmerken van een Traditioneel Bedrijf:</h3>
+
+          <ul className={styles.results__card__list}>
+            <li>
+              <b>Aandeelhouderschap</b>: De aandeelhouders bezitten het bedrijf en hun stemrecht is vaak gerelateerd aan
+              het aantal aandelen dat ze hebben.
+            </li>
+            <li>
+              <b>Winstgericht</b>: Het primaire doel is het maximaliseren van de winst voor aandeelhouders.
+            </li>
+            <li>
+              <b>Top-down Management</b>: Beslissingen worden genomen door hoger management en doorgevoerd binnen de
+              organisatie.
+            </li>
+            <li>
+              <b>Groei en Uitbreiding</b>: De focus ligt vaak op het vergroten van marktaandeel en het verhogen van de
+              omzet.
+            </li>
+            <li>
+              <b>Marktcompetitie</b>: Sterke focus op het concurreren in de markt om de beste producten of diensten te
+              leveren tegen de meest winstgevende prijzen.
+            </li>
+          </ul>
+
+          <section className={styles.results__card__section}>
+            <p>
+              Als eigenaar of manager binnen een traditioneel bedrijf heb je de kans om de strategische richting te
+              bepalen en de groei van het bedrijf te sturen. Deze bedrijfsvorm biedt structuur en duidelijke
+              richtlijnen, wat bijdraagt aan efficiëntie en effectieve besluitvorming.
+            </p>
+          </section>
+        </section>
+      </div>
+
+      <div className={styles.results__images}>
+        <div className={styles.results__images__wrapper}>
+          <Image src={imageProfitFirst} alt="Profit first" className={styles.results__images__wrapper__image} />
+        </div>
+
+        <div className={styles.results__images__wrapper}>
+          <Image src={imageProfitSecond} alt="Profit second" className={styles.results__images__wrapper__image} />
+        </div>
       </div>
 
       <ul className={styles.results__list}>
         {questions.map((question, index) => (
           <ResultsItem
             key={index}
+            index={index}
             question={question}
             answer={answers.find((answer) => {
               return answer.uuid === question.uuid;
@@ -75,60 +101,23 @@ export default function Results({ answers, questions }: ResultsProps) {
 }
 
 type ResultsItemProps = {
+  index: number;
   answer?: Answer;
   question: Question;
 };
 
-function ResultsItem({ answer, question }: ResultsItemProps) {
+function ResultsItem({ index, answer, question }: ResultsItemProps) {
   const choice = question.options?.find((option) => option.value === answer?.value);
 
   return (
     <li className={styles.results__list__item}>
       <div className={styles.results__list__item__wrapper}>
+        <h4>Vraag {index + 1}</h4>
         <h3 className={styles.results__list__item__wrapper__title}>{question.title}</h3>
 
         {choice && (
           <>
-            <ul className={styles.results__list__item__wrapper__impact}>
-              <li className={styles.results__list__item__wrapper__impact__item}>
-                <Image
-                  className={styles.results__list__item__wrapper__impact__item__icon}
-                  src={moneyIcon}
-                  alt="Money icon"
-                />
-
-                <span
-                  className={styles.results__list__item__wrapper__impact__item__slider}
-                  style={{ width: `${50 * choice!.impact.employeeWellbeing + 50}%` }}
-                ></span>
-              </li>
-
-              <li className={styles.results__list__item__wrapper__impact__item}>
-                <Image
-                  className={styles.results__list__item__wrapper__impact__item__icon}
-                  src={workersIcon}
-                  alt="Workers icon"
-                />
-
-                <span
-                  className={styles.results__list__item__wrapper__impact__item__slider}
-                  style={{ width: `${50 * choice!.impact.financialPresentation + 50}%` }}
-                ></span>
-              </li>
-
-              <li className={styles.results__list__item__wrapper__impact__item}>
-                <Image
-                  className={styles.results__list__item__wrapper__impact__item__icon}
-                  src={societyIcon}
-                  alt="Society icon"
-                />
-
-                <span
-                  className={styles.results__list__item__wrapper__impact__item__slider}
-                  style={{ width: `${50 * choice!.impact.societalImpact + 50}%` }}
-                ></span>
-              </li>
-            </ul>
+            <ResultsImpact impact={choice.impact} />
 
             <p className={styles.results__list__item__wrapper__description}>{choice.explain}</p>
           </>
