242aeb4407cc5e0890c546654958009e7132f4ae
Sjors van Holst <sjorsvanholst@gmail.com>
2024-01-18 21:03:31 +0100

    🚑️ Removed fs for Vercel

--- api/open-ai.ts
+++ api/open-ai.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { readFileSync } from "fs";
+// import { readFileSync } from "fs";
 import { Company, Question } from "../types";
 
 import questionJSON from "../examples.json";
@@ -12,8 +12,10 @@ const OPENAI_MODEL = process.env["OPENAI_MODEL"]!;
 
 const openai = new OpenAI({ apiKey: OPENAI_KEY });
 
+// const questionPrompt = readFileSync("prompt.txt", "utf8");
+
 const questionObject = JSON.stringify(questionJSON);
-const questionPrompt = readFileSync("prompt.txt", "utf8");
+const questionPrompt = `We gaan meeslepende vragen ontwerpen voor een 'kies je eigen avontuur'-spel, gefocust op het opzetten en uitbouwen van je eigen onderneming. Je ontvangt basisinformatie zoals de bedrijfsnaam, bedrijfsactiviteiten en de missie. Daarnaast moet je voor elke vraag een korte titel bedenken die de vraag samenvat.\n\nDe vragen zullen een verhaal ontwikkelen, beginnend bij de oprichting van het bedrijf of evolueren naar een climax, zoals een overnamebod. Het spel stimuleert spelers om na te denken over verschillende bedrijfsmodellen - van traditionele winstgerichte modellen tot maatschappelijk verantwoorde alternatieven zoals Worker Cooperatives of Steward Ownership met aandacht voor sociale impact en klimaatactie.\n\nBelangrijke bedrijfsmomenten worden belicht, zoals het openen van de eerste winkel, ethische besluitvorming en groeistrategieën. Het slot vraagstuk bepaalt welk bedrijfsmodel het best aansluit bij de keuzes en waarden van de speler, zoals ervaren tijdens hun reis.\n\nElke vraag biedt maximaal twee keuzes, weerspiegelend diverse bedrijfsfilosofieën en -strategieën, geïnformeerd door de begin informatie over het bedrijf. Je moet bij elke keuze een schatting doen van de impact van het antwoord. Dus financiële prestaties, duurzaamheid en maatschappelijke verantwoordelijkheid en tenslotte werknemers tevredenheid en - betrokkenheid.\n\nDit moet een getal tussen -1 en 1 zijn, maar zorg ervoor dat het nooit 0 is. De naam van deze variablen zijn "financialPresentation", "employeeWellbeing", en "societalImpact"\n\nDaarnaast moet je ook een korte uitleg geven over waarom deze score gegeven is en de consequenties van de acties. Als de bedrijfsmissie nog niet bekend is hoef je hier niet naar te vragen, het systeem zelf zal deze vraag de volgende ronde stellen.\n\nLater worden de antwoorden op deze vragen gemeten in de metrics financiële prestatie, duurzaamheid en maatschappelijke verantwoordelijkheid en werknemerstevredenheid en -betrokkenheid. Zorg ervoor dat de impact van de verschillende vragen en keuzes duidelijk is voor de gebruiker. De vragen mogen onrealistisch of fictief zijn.\n\n`;
 
 export async function fetchQuestion(
   company: Company,
